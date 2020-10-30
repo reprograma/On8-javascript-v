4ddd537c9d01aaa26e118308f9cd726b38264a23
Biamichelle <biamichellelivre@gmail.com>
2020-10-30 18:41:50 -0300

    exericio para casa

---
+++ exercicio-para-casa/biamichelle/perguntas.js
@@ -0,0 +1,100 @@
+/*
+ 1. O que é um Objeto? Por que ele é tão importante na linguagem JavaScript?
+ 
+*/
+
+/*
+  Crie um exemplo de um objeto
+*/
+
+
+
+
+/*
+ 2. Qual é a diferença entre uma função e uma Arrow Function?
+
+*/
+
+/*
+ Crie um exemplo de Arrow Function
+*/
+
+
+
+
+/*
+ 3. Como funciona o destructuring?
+*/
+
+/*
+ Crie um exemplo de destructuring de objeto e array
+*/
+
+
+
+
+/*
+ 4. Para que serve o Spread e Rest?
+*/
+
+
+/*
+  Crie um exemplo como utilizar Spred e Rest
+*/
+
+
+
+
+/*
+ 5. Quais as vantagens de utilizar o código em modules em JavaScript ?
+*/
+
+/*
+ Crie um exemplo utilizando module 
+*/
+
+
+
+
+/*
+ 6. O que é o Fetch? Quando utilizamos ele?
+*/
+
+/*
+ Crie um exemplo utilizando Fetch
+*/
+
+
+
+
+/*
+ 7. Qual a vantagem de utilizar Async/Await?
+*/
+
+/*
+ Utilizando o exemplo do Fetch, refatore utilizando o async await.
+*/
+
+
+
+
+/*
+ 8. Quando devemos utilizar o Map, Filter e o Reduce?
+*/
+
+/*
+ Crie um exemplo utilizando Map, Filter e o Reduce
+*/
+
+
+
+
+/*
+  9. Quando utilizamos as Expressões?
+*/
+
+/*
+  Crie um exemplo de uma Expressão utilizando o operador &&
+*/
+
+
